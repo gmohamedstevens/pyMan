8dc21efdfc314fc5f8dfd554fde77e9ef0a1ec46
Gamal Mohamed <gmohamed@stevens.edu>
2021-01-20 17:10:43 -0500

    Added requests library for importing dictionary from url

--- pyMan.py
+++ pyMan.py
@@ -2,9 +2,13 @@
 # INITIALIZATION #
 ##################
 import random
+import requests
 
-MAX_TRIES = 5
-DICTIONARY = ("secret", "python", "guess", "clue")
+# Get dictionary from word list URL
+WORD_SITE = "https://www.mit.edu/~ecprice/wordlist.10000"
+RESPONSE = requests.get(WORD_SITE)
+DICTIONARY = RESPONSE.content.splitlines() # Gets list of words from URL of bytes type
+MAX_TRIES = 7
 
 #############
 # FUNCTIONS #
@@ -12,7 +16,7 @@ DICTIONARY = ("secret", "python", "guess", "clue")
 
 # Return a random word from the dictionary
 def get_word():
-    return random.choice(DICTIONARY)
+    return random.choice(DICTIONARY).decode('UTF-8') # Decode byte type to string type
 
 # Show an intro message to the player
 def show_intro():
@@ -26,10 +30,6 @@ def show_game_prompt():
     print("Already guessed letters: ", guessed_letters)
     return input("Please guess a letter: ")
 
-# Return if the passed letter is contained in the passed word
-def letter_is_in_word(letter, word):
-    return letter in word
-
 # Returns if the guess made is valid
 def valid_guess_made():
     if not len(letter_guess) == 1: # Guess must be only one character
@@ -39,7 +39,7 @@ def valid_guess_made():
         if not letter_guess.isalpha(): # Guess must be in the alphabet
             print("Invalid guess, that is not a letter!")
             return False
-        if letter_is_in_word(letter_guess, guessed_letters): # Cannot guess the same letter twice
+        if letter_guess in guessed_letters: # Cannot guess the same letter twice
             print("Invalid guess, you already guessed that letter!")
             return False
     return True
@@ -62,14 +62,14 @@ while True: # Begin main loop
         print()
         if valid_guess_made():
             guessed_letters = guessed_letters + letter_guess + " " # Add guessed letter to list
-            if letter_is_in_word(letter_guess, secret_word):
+            if letter_guess in secret_word:
                 print("Correct!") # Guess is correct; reveal the hidden letters
                 for element in range(0, len(secret_word)):
                     if letter_guess == secret_word[element]:
                         temp_word = list(display_word)
                         temp_word[element * 2] = secret_word[element]
                         display_word = "".join(temp_word)
-                if not letter_is_in_word("_", display_word): # If there are no more hidden letters, the player has won
+                if not "_" in display_word: # If there are no more hidden letters, the player has won
                     print("You guessed the secret word!")
                     player_won = True
             else:
